7d42c5b08182a1ca49a68c434e5dcff8ead0a715
Dennis Ting <dennisting@gmail.com>
2015-09-26 17:11:14 -0700

    Initial commit.

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('appApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
