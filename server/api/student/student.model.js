167eb0bf98816c9fa376417a93ca784e783730ed
Patrick San Juan <pat.sanjuan@gmail.com>
2015-10-05 21:19:13 -0700

    nested object in schema

--- server/api/student/student.model.js
+++ server/api/student/student.model.js
@@ -3,25 +3,17 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-var SchoolYearSchema = new Schema({
-		SchoolYear: Number,
-		AllAbsences: Number,
-		Tardy: Number,
-		Present: Number,
-		Enrolled: Number
-});
-
 var StudentSchema = new Schema({
   	_id: Number, // Student ID
-  	lastName: String,
-  	firstName: String,
-  	statistics: [SchoolYearSchema]
+  	LastName: String,
+  	FirstName: String,
+  	Statistics: [{
+  		SchoolYear: Number,
+  		AllAbsences: Number,
+  		Tardy: Number,
+  		Present: Number,
+  		Enrolled: Number
+  	}]
 });
 
-var SchoolYear = mongoose.model('SchoolYear', SchoolYearSchema);
-var Student = mongoose.model('Student', StudentSchema);
-
-module.exports = {
-		SchoolYear: SchoolYear,
-		Student: Student
-};
+module.exports = mongoose.model('Student', StudentSchema);
