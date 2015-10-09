0360dc8eccdd8220cdce3200d6d8e9b1fb0973e0
Dennis Ting <dennisting@gmail.com>
2015-10-09 04:20:55 -0700

    Add redirects and views for forbidden states.
    Resolves #15

--- client/app/app.js
+++ client/app/app.js
@@ -7,18 +7,19 @@ angular.module('app', [
   'ui.router',
   'ui.bootstrap'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
-    $urlRouterProvider
-      .otherwise('/');
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider,
+    $httpProvider) {
+    $urlRouterProvider.otherwise('/');
 
     $locationProvider.html5Mode(true);
     $httpProvider.interceptors.push('authInterceptor');
   })
 
-  .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $injector) {
+  .factory('authInterceptor',
+  function($rootScope, $q, $cookieStore, $injector) {
     return {
       // Add authorization token to headers
-      request: function (config) {
+      request: function(config) {
         config.headers = config.headers || {};
         if ($cookieStore.get('token')) {
           config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
@@ -28,7 +29,7 @@ angular.module('app', [
 
       // Intercept 401s and redirect you to login
       responseError: function(response) {
-        if(response.status === 401) {
+        if (response.status === 401) {
           // remove any stale tokens
           $cookieStore.remove('token');
           // Use injector to get around circular dependency.
@@ -41,13 +42,32 @@ angular.module('app', [
     };
   })
 
-  .run(function ($rootScope, $state, Auth) {
-    // Redirect to login if route requires auth and you're not logged in
-    $rootScope.$on('$stateChangeStart', function (event, next) {
+  .run(function($rootScope, $state, Auth, Role) {
+    $rootScope.$on('$stateChangeStart', function(event, next) {
       Auth.isLoggedInAsync(function(loggedIn) {
-        if (!loggedIn && next.name !== 'login') {
-          event.preventDefault();
-          $state.go('login');
+        if (!loggedIn) {
+          if (next.name !== 'login') {
+            // Redirect to login if route requires auth and user not logged in
+            event.preventDefault();
+            $state.go('login');
+          }
+        } else {
+          var role = Auth.getCurrentUser().role;
+          if (!Role.hasRole(role, 'teacher')) {
+            if (next.name !== 'access.guest') {
+              // Redirect to guest state if user not at least a teacher
+              event.preventDefault();
+              $state.go('access.guest');
+            }
+          } else if (next.auth) {
+            var requiredRole = next.auth.required;
+            if (!Role.hasRole(role, requiredRole) &&
+                next.name !== 'access.forbidden') {
+              // Redirect to forbidden state if user not at a high enough role
+              event.preventDefault();
+              $state.go('access.forbidden', {required: requiredRole});
+            }
+          }
         }
       });
     });
