fc9292865f10cb6721c485de60ad05b7e0a5d547
Patrick San Juan <pat.sanjuan@gmail.com>
2015-10-13 14:48:56 -0700

    initial build

---
+++ client/app/main/PDFUpload/PDFUpload.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('app')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('PDFUpload', {
+        url: '/PDFUpload',
+        parent: 'main',
+        templateUrl: 'app/main/PDFUpload/PDFUpload.html',
+        controller: 'PDFUploadCtrl'
+      });
+  });
