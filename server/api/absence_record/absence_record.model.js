4ea994b950a5f1673d66ffd3f799c3190b689724
Dennis Ting <dennisting@gmail.com>
2015-10-16 23:33:12 -0700

    Split statistics subdoc out of StudentSchema
    - Resolves #54

---
+++ server/api/absence_record/absence_record.model.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var AbsenceRecordSchema = new Schema({
+  schoolYear: {type: String, required: true},
+  school: {type: Schema.Types.ObjectId, ref: 'School', required: true},
+  entries: [{
+    student: {type: Schema.Types.ObjectId, ref: 'Student', required: true},
+    absences: {type: Number, required: true},
+    tardies: {type: Number, required: true},
+    present: {type: Number, required: true},
+    enrolled: {type: Number, required: true}
+  }]
+});
+
+AbsenceRecordSchema.virtual('created_at').get(function() {
+  return this._id.getTimestamp();
+});
+
+module.exports = mongoose.model('AbsenceRecord', AbsenceRecordSchema);
---
+++ server/api/absence_record/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./absence_record.controller');
+var auth = require('../../auth/auth.service');
+
+var router = express.Router();
+
+// TODO: Add "assignment" authorization checks.
+
+router.get('/', auth.hasRole('manager'), controller.index);
+router.get('/:id', auth.hasRole('teacher'), controller.show);
+router.post('/', auth.hasRole('teacher'), controller.create);
+router.put('/:id', auth.hasRole('teacher'), controller.update);
+router.patch('/:id', auth.hasRole('teacher'), controller.update);
+router.delete('/:id', auth.hasRole('teacher'), controller.destroy);
+
+module.exports = router;
