919ba0af584a94eac2f5b54953517721e063696a
Dennis Ting <dennisting@gmail.com>
2015-10-20 10:19:56 -0700

    Fix AuthService jwt expiresIn deprecation warning
    This resolves #68

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -90,8 +90,7 @@ function schoolAuth() {
  * Returns a jwt token signed by the app secret
  */
 function signToken(id) {
-  return jwt.sign({_id: id}, config.secrets.session,
-    {expiresInMinutes: 60 * 5});
+  return jwt.sign({_id: id}, config.secrets.session, {expiresIn: 60 * 5});
 }
 
 /**
