af8ca277a3ab3d2576dcddc9c731f56f2ca36cd3
Dennis Ting <dennisting@gmail.com>
2015-10-23 21:47:18 -0700

    Update Lodash to v3
    - Run `npm install && bower install` after pulling this change

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
       'client/bower_components/angular-sanitize/angular-sanitize.js',
       'client/bower_components/angular-ui-grid/ui-grid.js',
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
-      'client/bower_components/lodash/dist/lodash.compat.js',
+      'client/bower_components/lodash/lodash.js',
       'client/bower_components/matchmedia-ng/matchmedia-ng.js',
       'client/bower_components/ui-select/dist/select.js',
       'client/app/app.js',
