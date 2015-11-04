6845f75a5867cd54f29f2b68c37348bd6ec90b73
Patrick San Juan <pat.sanjuan@gmail.com>
2015-11-04 14:56:48 -0800

    finished absence record and student record functions

--- client/app/main/pdf-upload/pdf-upload.controller.js
+++ client/app/main/pdf-upload/pdf-upload.controller.js
@@ -3,7 +3,6 @@
 angular.module('app')
 	.controller('PDFUploadCtrl', function($scope, Upload, $timeout) {
 		$scope.submit = function(file) {
-			console.log(file);
 			file.upload = Upload.upload({
 				url: '/api/pdfs/',
 				data: {
@@ -14,7 +13,6 @@ angular.module('app')
 			file.upload.then(function(response) {
 				$timeout(function() {
 					file.result = response.data;
-					console.log(response);
 					if(response.status === 204) {
 						$scope.message = 'File Uploaded!'
 					} else {
