c6473fafd40657ead6fdf37e92c439d61f199f07
Dennis Ting <dennisting@gmail.com>
2015-11-05 08:05:09 -0800

    Fix build errors.
    Missing semicolons and some other jshint errors that
    prevent grunt from building fixed.

--- client/app/main/pdf-upload/pdf-upload.controller.js
+++ client/app/main/pdf-upload/pdf-upload.controller.js
@@ -1,25 +1,26 @@
 'use strict';
 
 angular.module('app')
-	.controller('PDFUploadCtrl', function($scope, Upload, $timeout) {
-		$scope.submit = function(file) {
-			file.upload = Upload.upload({
-				url: '/api/pdfs/',
-				data: {
-					file: file
-				}
-			});
+  .controller('PDFUploadCtrl', function($scope, Upload, $timeout) {
+    $scope.submit = function(file) {
+      file.upload = Upload.upload({
+        url: '/api/pdfs/',
+        data: {
+          file: file
+        }
+      });
 
-			file.upload.then(function(response) {
-				$timeout(function() {
-					file.result = response.data;
-					if(response.status === 204) {
-						$scope.message = 'File Uploaded!'
-					} else {
-						$scope.errorMessage = response.status	+ ': ' + response.statusText;
-					}
-					form.reset();
-				});
-			});
-		};
-	});
+      file.upload.then(function(response) {
+        $timeout(function() {
+          file.result = response.data;
+          if (response.status === 204) {
+            $scope.message = 'File Uploaded!';
+          } else {
+            $scope.errorMessage = response.status + ': ' + response.statusText;
+          }
+          // TODO: This causes build jshint errors. Find out how to do this.
+          // form.reset();
+        });
+      });
+    };
+  });
