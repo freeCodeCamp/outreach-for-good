dd23c7e97f24c40edf91f27e2044e423ec6d56b9
Dennis Ting <dennisting@gmail.com>
2015-12-12 01:25:53 -0800

    Update grid in dashboard to group by schools
    If a teacher logs in the school column is omitted.
    If a manager+ logs in the students are grouped by school.

--- client/app/main/dashboard/dashboard.controller.js
+++ client/app/main/dashboard/dashboard.controller.js
@@ -2,12 +2,12 @@
 
 var app = angular.module('app');
 
-app.controller('DashboardCtrl', function($scope, Data) {
-
+function DashboardCtrl($scope, Data, Auth, uiGridGroupingConstants) {
   $scope.studentGridOptions = {
     enableSorting: true,
     enableGridMenu: true,
-    enableFiltering: true
+    enableFiltering: true,
+    treeRowHeaderAlwaysVisible: false
   };
 
   $scope.studentGridOptions.columnDefs = [{
@@ -30,11 +30,13 @@ app.controller('DashboardCtrl', function($scope, Data) {
   }, {
     name: 'entries.absences',
     displayName: 'Absences',
-    minWidth: 150
+    minWidth: 150,
+    treeAggregationType: uiGridGroupingConstants.aggregation.SUM
   }, {
     name: 'entries.tardies',
     displayName: 'Tardies',
-    minWidth: 150
+    minWidth: 150,
+    treeAggregationType: uiGridGroupingConstants.aggregation.SUM
   }, {
     name: 'entries.present',
     displayName: 'Present',
@@ -43,11 +45,19 @@ app.controller('DashboardCtrl', function($scope, Data) {
     name: 'entries.enrolled',
     displayName: 'Enrolled',
     minWidth: 150
-  }, {
-    name: 'school.name',
-    displayName: 'School Name',
-    minWidth: 150
   }];
+
+  if (Auth.getCurrentUser().role !== 'teacher') {
+    $scope.studentGridOptions.columnDefs.push({
+      name: 'school.name',
+      displayName: 'School Name',
+      minWidth: 150,
+      grouping: {groupPriority: 0},
+      sort: {priority: 0, direction: 'asc'}
+    });
+  } else {
+    $scope.assignment = Auth.getCurrentUser().assignment;
+  }
 
   $scope.studentGridOptions.onRegisterApi = function(gridApi) {
     $scope.studentGridApi = gridApi;
@@ -59,4 +69,6 @@ app.controller('DashboardCtrl', function($scope, Data) {
   $scope.home = 0;
   $scope.court = 4;
   $scope.sst = 20;
-});
+}
+
+app.controller('DashboardCtrl', DashboardCtrl);
