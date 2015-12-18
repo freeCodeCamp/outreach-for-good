55f77e84eb0a83f38c790ef20d90b9a4d33982bb
Dennis Ting <dennisting@gmail.com>
2015-12-17 21:17:46 -0800

    Change status to trigger and action date fields

--- server/api/intervention/intervention.model.js
+++ server/api/intervention/intervention.model.js
@@ -3,22 +3,26 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
-var types = ['Phone', 'Letter', 'Home', 'SST', 'Court'];
+var types = [
+  'Phone Call',
+  'Letter Sent',
+  'Home Visit',
+  'SST Referral',
+  'Court Referral'
+];
 
 var InterventionSchema = new Schema({
+  type: {type: String, required: true, enum: types},
+  tier: {type: Number, required: true},
+
   student: {type: Schema.Types.ObjectId, ref: 'Student', required: true},
   school: {type: Schema.Types.ObjectId, ref: 'School', required: true},
+
   record: {type: Schema.Types.ObjectId, ref: 'AbsenceRecord', required: true},
-  type: {type: String, required: true, enum: types},
+  schoolYear: {type: String, required: true},
+  triggerDate: {type: Date, required: true},
 
-  status: {type: String, required: true},
-  statusLog: [{
-    oldStatus: {type: String},
-    newStatus: {type: String, required: true},
-    date: {type: Date, required: true},
-    user: {type: Schema.Types.ObjectId, ref: 'User', required: true}
-  }],
-
+  actionDate: {type: Date},
   notes: [{
     note: {type: String, required: true},
     user: {type: Schema.Types.ObjectId, ref: 'User', required: true}
