b721b2840600fca331674ee2ec4d92e49bc93e8e
Dennis Ting <dennisting@gmail.com>
2016-01-09 02:31:40 -0800

    Add dev module for development tasks
    Resolves #155
    
    Utility module that will want to be removed or
    disabled in production.
    
    For now just includes a route to reset all
    collections in the mongodb except for the users.

--- server/routes.js
+++ server/routes.js
@@ -9,19 +9,23 @@ var path = require('path');
 
 module.exports = function(app) {
 
-  // Insert routes below
+  // models
   app.use('/api/absence-records', require('./api/absence-record'));
   app.use('/api/interventions', require('./api/intervention'));
-  app.use('/api/pdfs', require('./api/pdf'));
   app.use('/api/schools', require('./api/school'));
   app.use('/api/students', require('./api/student'));
   app.use('/api/users', require('./api/user'));
 
+  // services
   app.use('/auth', require('./auth'));
+  app.use('/api/pdfs', require('./api/pdf'));
+
+  // development
+  app.use('/api/devs', require('./api/dev'));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+    .get(errors[404]);
 
   // All other routes should redirect to the index.html
   app.route('/*')
