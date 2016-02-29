5ce90837d107ab8fbf9bfc683cd3a032f4d62c4d
Patrick San Juan <pat.sanjuan@gmail.com>
2016-02-29 14:10:28 -0800

    updated chart properties

--- client/app/main/school-reports/school-reports.controller.js
+++ client/app/main/school-reports/school-reports.controller.js
@@ -3,10 +3,46 @@
 var app = angular.module('app');
 
 function SchoolReports($scope, uiGridGroupingConstants) {
-	$scope.studentGridOptions = {};
-	$scope.chronicAbsentGridOptions = {};
-	$scope.dalyAtttendanceGridOptions = {};
-	$scope.firstCallGridOptions = {};
+	$scope.chronicAbsentGridOptions = {
+		enableSorting: true,
+    enableGridMenu: true,
+    enableFiltering: true,
+    treeRowHeaderAlwaysVisible: false,
+    exporterPdfDefaultStyle: {fontSize: 9},
+    exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, color: 'grey'},
+    exporterPdfHeader: { text: 'Chronically Absent Students', style: 'headerStyle' },
+    exporterPdfOrientation: 'landscape',
+    exporterPdfPageSize: 'LETTER',
+    exporterPdfMaxGridWidth: 500,
+    exporterPdfFooter: function ( currentPage, pageCount ) {
+      return { text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle' };
+    },
+    exporterPdfCustomFormatter: function ( docDefinition ) {
+      docDefinition.styles.headerStyle = { fontSize: 22, bold: true, color: '#265E6D' };
+      docDefinition.styles.footerStyle = { fontSize: 10, bold: true };
+      return docDefinition;
+    }
+	};
+	$scope.atRiskChronicAbsentGridOptions = {
+		enableSorting: true,
+    enableGridMenu: true,
+    enableFiltering: true,
+    treeRowHeaderAlwaysVisible: false,
+    exporterPdfDefaultStyle: {fontSize: 9},
+    exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, color: 'grey'},
+    exporterPdfHeader: { text: 'At Risk Chronically absent students', style: 'headerStyle' },
+    exporterPdfOrientation: 'landscape',
+    exporterPdfPageSize: 'LETTER',
+    exporterPdfMaxGridWidth: 500,
+    exporterPdfFooter: function ( currentPage, pageCount ) {
+      return { text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle' };
+    },
+    exporterPdfCustomFormatter: function ( docDefinition ) {
+      docDefinition.styles.headerStyle = { fontSize: 22, bold: true, color: '#265E6D' };
+      docDefinition.styles.footerStyle = { fontSize: 10, bold: true };
+      return docDefinition;
+    }
+	};
 }
 
 app.controller('SchoolReports', SchoolReports);
