3b862c6d861fa5e33b6a7ef1f794e876db0899a5
Dennis Ting <dennisting@gmail.com>
2016-03-02 11:41:48 -0800

    Fix some formatting issues

--- client/app/main/school-reports/school-reports.controller.js
+++ client/app/main/school-reports/school-reports.controller.js
@@ -3,47 +3,61 @@
 var app = angular.module('app');
 
 function SchoolReports($scope, Auth, uiGridGroupingConstants,
-	Student, AbsenceRecord) {
-	$scope.chronicAbsentGridOptions = {
-		enableSorting: true,
+  Student, AbsenceRecord) {
+  $scope.chronicAbsentGridOptions = {
+    enableSorting: true,
     enableGridMenu: true,
     enableFiltering: true,
     treeRowHeaderAlwaysVisible: false,
     exporterPdfDefaultStyle: {fontSize: 9},
     exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, color: 'grey'},
-    exporterPdfHeader: { text: 'Chronically Absent Students', style: 'headerStyle' },
+    exporterPdfHeader: {
+      text: 'Chronically Absent Students',
+      style: 'headerStyle'
+    },
     exporterPdfOrientation: 'landscape',
     exporterPdfPageSize: 'LETTER',
     exporterPdfMaxGridWidth: 500,
-    exporterPdfFooter: function ( currentPage, pageCount ) {
-      return { text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle' };
+    exporterPdfFooter: function(currentPage, pageCount) {
+      return {
+        text: currentPage.toString() + ' of ' + pageCount.toString(),
+        style: 'footerStyle'
+      };
     },
-    exporterPdfCustomFormatter: function ( docDefinition ) {
-      docDefinition.styles.headerStyle = { fontSize: 22, bold: true, color: '#265E6D' };
-      docDefinition.styles.footerStyle = { fontSize: 10, bold: true };
+    exporterPdfCustomFormatter: function(docDefinition) {
+      docDefinition.styles.headerStyle =
+      {fontSize: 22, bold: true, color: '#265E6D'};
+      docDefinition.styles.footerStyle = {fontSize: 10, bold: true};
       return docDefinition;
     }
-	};
-	$scope.atRiskChronicAbsentGridOptions = {
-		enableSorting: true,
+  };
+  $scope.atRiskChronicAbsentGridOptions = {
+    enableSorting: true,
     enableGridMenu: true,
     enableFiltering: true,
     treeRowHeaderAlwaysVisible: false,
     exporterPdfDefaultStyle: {fontSize: 9},
     exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, color: 'grey'},
-    exporterPdfHeader: { text: 'At Risk Chronically absent students', style: 'headerStyle' },
+    exporterPdfHeader: {
+      text: 'At Risk Chronically absent students',
+      style: 'headerStyle'
+    },
     exporterPdfOrientation: 'landscape',
     exporterPdfPageSize: 'LETTER',
     exporterPdfMaxGridWidth: 500,
-    exporterPdfFooter: function ( currentPage, pageCount ) {
-      return { text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle' };
+    exporterPdfFooter: function(currentPage, pageCount) {
+      return {
+        text: currentPage.toString() + ' of ' + pageCount.toString(),
+        style: 'footerStyle'
+      };
     },
-    exporterPdfCustomFormatter: function ( docDefinition ) {
-      docDefinition.styles.headerStyle = { fontSize: 22, bold: true, color: '#265E6D' };
-      docDefinition.styles.footerStyle = { fontSize: 10, bold: true };
+    exporterPdfCustomFormatter: function(docDefinition) {
+      docDefinition.styles.headerStyle =
+      {fontSize: 22, bold: true, color: '#265E6D'};
+      docDefinition.styles.footerStyle = {fontSize: 10, bold: true};
       return docDefinition;
     }
-	};
+  };
   $scope.chronicAbsentGridOptions.columnDefs = [{
     name: 'entries.student.studentId',
     displayName: 'Student Id',
