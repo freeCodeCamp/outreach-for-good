5e951b5f2a7f13795fc8e550baff7bda853fc8d3
Dennis Ting <dennisting@gmail.com>
2016-03-01 21:20:29 -0800

    Remove unused routes for Intervention endpoint

--- server/api/intervention/index.js
+++ server/api/intervention/index.js
@@ -6,16 +6,8 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', auth.hasRole('teacher'), controller.index);
 router.get('/current', auth.hasRole('teacher'), controller.current);
 router.post('/:id/note', auth.hasRole('teacher'), controller.addNote);
 router.put('/:id/action', auth.hasRole('teacher'), controller.updateAction);
-
-// TODO: Add role and assignment authorization checks.
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
 
 module.exports = router;
--- server/api/intervention/intervention.controller.js
+++ server/api/intervention/intervention.controller.js
@@ -4,24 +4,16 @@ var _ = require('lodash');
 var auth = require('../../auth/auth.service');
 var Intervention = require('./intervention.model');
 
-// Get list of interventions
-exports.index = function(req, res) {
-  Intervention.find(function(err, interventions) {
-    if (err) return handleError(res, err);
-    return res.status(200).json(interventions);
-  });
-};
-
 /**
- * Get current unmarked interventions.
+ * Get current unmarked intervention counts.
  * restriction: 'teacher'
  *
  * Returns an aggregation for entries based on the req user role:
- * - teachers will get interventions for assignment school
- * - manager+ will get interventions for all schools
+ * - teachers will get intervention counts for assignment school
+ * - manager+ will get intervention counts for all schools
  */
 exports.current = function(req, res) {
-  var options = [{
+  var pipeline = [{
     $match: {actionDate: null}
   }, {
     $group: {
@@ -30,67 +22,18 @@ exports.current = function(req, res) {
     }
   }];
   if (req.user.role === 'teacher') {
-    options[0].$match.school = req.user.assignment;
+    pipeline[0].$match.school = req.user.assignment;
   }
-  Intervention.aggregate(options, function(err, results) {
+  Intervention.aggregate(pipeline, function(err, results) {
     if (err) return handleError(res, err);
     return res.status(200).json(results);
   });
 };
 
-// Get a single intervention
-exports.show = function(req, res) {
-  Intervention.findById(req.params.id, function(err, intervention) {
-    if (err) return handleError(res, err);
-    if (!intervention) return res.status(404).send('Not Found');
-    return res.status(200).json(intervention);
-  });
-};
-
-// Creates a new intervention in the DB.
-exports.create = function(req, res) {
-  Intervention.create(req.body, function(err, intervention) {
-    if (err) return handleError(res, err);
-    return res.status(201).json(intervention);
-  });
-};
-
-// Updates an existing intervention in the DB.
-exports.update = function(req, res) {
-  if (req.body._id) delete req.body._id;
-  Intervention.findById(req.params.id, function(err, intervention) {
-    if (err) return handleError(res, err);
-    if (!intervention) return res.status(404).send('Not Found');
-    var updated = _.merge(intervention, req.body);
-    updated.save(function(err) {
-      if (err) return handleError(res, err);
-      return res.status(200).json(intervention);
-    });
-  });
-};
-
-// Updates an existing intervention's actionDate in the DB.
-exports.updateAction = function(req, res) {
-  Intervention
-    .findById(req.params.id)
-    .populate('student')
-    .exec(function(err, intervention) {
-      if (err) return handleError(res, err);
-      if (!intervention) return res.status(404).send('Not Found');
-      if (!auth.authorizeStudent(intervention.student, req)) {
-        return res.status(403).json({
-          reason: auth.schoolMsg(req.user.assignment || 'None')
-        });
-      }
-      intervention.actionDate = req.body.actionDate;
-      intervention.save(function(err) {
-        if (err) return handleError(res, err);
-        return res.status(200).json(intervention);
-      });
-    });
-};
-
-// Add a note to an intervention.
+/**
+ * Add a note to an intervention.
+ * restriction: 'teacher'
+ */
 exports.addNote = function(req, res) {
   Intervention
     .findById(req.params.id)
@@ -114,16 +57,28 @@ exports.addNote = function(req, res) {
     });
 };
 
-// Deletes a intervention from the DB.
-exports.destroy = function(req, res) {
-  Intervention.findById(req.params.id, function(err, intervention) {
-    if (err) return handleError(res, err);
-    if (!intervention) return res.status(404).send('Not Found');
-    intervention.remove(function(err) {
+/**
+ * Update actionDate for an intervention.
+ * restriction: 'teacher'
+ */
+exports.updateAction = function(req, res) {
+  Intervention
+    .findById(req.params.id)
+    .populate('student')
+    .exec(function(err, intervention) {
       if (err) return handleError(res, err);
-      return res.status(204).send('No Content');
+      if (!intervention) return res.status(404).send('Not Found');
+      if (!auth.authorizeStudent(intervention.student, req)) {
+        return res.status(403).json({
+          reason: auth.schoolMsg(req.user.assignment || 'None')
+        });
+      }
+      intervention.actionDate = req.body.actionDate;
+      intervention.save(function(err) {
+        if (err) return handleError(res, err);
+        return res.status(200).json(intervention);
+      });
     });
-  });
 };
 
 function handleError(res, err) {
