cb17432039ac4a7fe32cdf3d9863c1a07d8bd302
Dennis Ting <dennisting@gmail.com>
2016-03-02 17:49:23 -0800

    Set explicity bodyParser limits in config
    Resolves #217

--- server/config/express.js
+++ server/config/express.js
@@ -24,8 +24,8 @@ module.exports = function(app) {
   app.engine('html', require('ejs').renderFile);
   app.set('view engine', 'html');
   app.use(compression());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({limit:'10mb', extended: true }));
+  app.use(bodyParser.json({limit: '10mb'}));
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
