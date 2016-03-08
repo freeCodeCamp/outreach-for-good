4a73011ddfb30b8e3468adda7896bc79b4e3b6a9
Dennis Ting <dennisting@gmail.com>
2016-03-08 15:52:58 -0800

    Change some names for outreach functions

--- server/api/outreach/index.js
+++ server/api/outreach/index.js
@@ -7,9 +7,9 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.post('/', auth.hasRole('teacher'), controller.create);
-router.put('/:id/archive', auth.hasRole('teacher'), controller.toggleArchive);
+router.put('/:id/archived', auth.hasRole('teacher'), controller.updateArchived);
 router.delete('/:id', auth.hasRole('teacher'), controller.delete);
 
-router.post('/:id/note', auth.hasRole('teacher'), controller.addNote);
+router.post('/:id/note', auth.hasRole('teacher'), controller.createNote);
 
 module.exports = router;
