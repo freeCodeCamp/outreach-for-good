ab068b06e75a45953a784b4858307bb51dfaa9b5
Patrick San Juan <pat.sanjuan@gmail.com>
2016-03-12 14:43:54 -0800

    login page fadein, sidebar toggle animate rotate

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,7 +1,16 @@
 'use strict';
 
 angular.module('app')
-  .controller('LoginCtrl', function ($scope, $window) {
+  .controller('LoginCtrl', function ($scope, $window, $timeout) {
+    $scope.isLoaded = false;
+
+    $scope.$on('$viewContentLoaded', 
+    	function(){
+    		$timeout(function() {
+    			$scope.isLoaded = true;
+    		}, 500);
+    	});
+
     $scope.loginOauth = function(provider) {
       $window.location.href = '/auth/' + provider;
     };
