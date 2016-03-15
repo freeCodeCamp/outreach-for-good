3e3e332058a25b11fe346f2f3347c905d61c6f2e
Patrick San Juan <pat.sanjuan@gmail.com>
2016-03-14 17:07:35 -0700

    refactored code

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -2,14 +2,11 @@
 
 angular.module('app')
   .controller('LoginCtrl', function ($scope, $window, $timeout) {
-    $scope.isLoaded = false;
-
-    $scope.$on('$viewContentLoaded', 
-    	function(){
-    		$timeout(function() {
-    			$scope.isLoaded = true;
-    		}, 500);
-    	});
+    $scope.$on('$viewContentLoaded', function() {
+      $timeout(function() {
+        $scope.isLoaded = true;
+      });
+    });
 
     $scope.loginOauth = function(provider) {
       $window.location.href = '/auth/' + provider;
