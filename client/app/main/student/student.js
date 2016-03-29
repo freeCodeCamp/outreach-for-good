ca079fd72fabaa72c27d78ac2c410dbb33ab750e
Dennis Ting <dennisting@gmail.com>
2016-03-29 16:00:56 -0700

    Reorganize student notes into its own subpackage

--- client/app/main/student/student.js
+++ client/app/main/student/student.js
@@ -1,19 +1,11 @@
 'use strict';
 
-angular.module('app')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('student', {
-        url: '/student/:studentId',
-        parent: 'main',
-        abstract: '.outreaches',
-        templateUrl: 'app/main/student/student.html',
-        controller: 'StudentCtrl'
-      })
-      .state('student.notes', {
-        url: '/notes',
-        parent: 'student',
-        templateUrl: 'app/main/student/partial/notes.html',
-        controller: 'StudentNotesCtrl'
-      });
+angular.module('app').config(function($stateProvider) {
+  $stateProvider.state('student', {
+    url: '/student/:studentId',
+    parent: 'main',
+    abstract: '.outreaches',
+    templateUrl: 'app/main/student/student.html',
+    controller: 'StudentCtrl'
   });
+});
