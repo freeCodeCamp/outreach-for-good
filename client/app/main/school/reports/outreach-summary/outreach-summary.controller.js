a838a286ed40b91fa053a1cb4af878ae9c1f0404
Dennis Ting <dennisting@gmail.com>
2016-04-07 03:02:08 -0700

    Remove PDF export and PDFMake
    Resolves #375.
    
    Running:
    
        bower prune
    
    should remove the pdfmake bower component after pulling the change.
    
    If that fails, try running:
    
        bower install --force

--- client/app/main/school/reports/outreach-summary/outreach-summary.controller.js
+++ client/app/main/school/reports/outreach-summary/outreach-summary.controller.js
@@ -11,27 +11,7 @@ function OutreachSummaryCtrl($scope, $timeout, uiGridGroupingConstants,
     enableGridMenu: true,
     enableFiltering: true,
     treeRowHeaderAlwaysVisible: false,
-    exporterPdfDefaultStyle: {fontSize: 9},
-    exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, color: 'grey'},
-    exporterPdfHeader: {
-      text: 'Outreach Summary',
-      style: 'headerStyle'
-    },
-    exporterPdfOrientation: 'landscape',
-    exporterPdfPageSize: 'LETTER',
-    exporterPdfMaxGridWidth: 500,
-    exporterPdfFooter: function(currentPage, pageCount) {
-      return {
-        text: currentPage.toString() + ' of ' + pageCount.toString(),
-        style: 'footerStyle'
-      };
-    },
-    exporterPdfCustomFormatter: function(docDefinition) {
-      docDefinition.styles.headerStyle =
-      {fontSize: 22, bold: true, color: '#265E6D'};
-      docDefinition.styles.footerStyle = {fontSize: 10, bold: true};
-      return docDefinition;
-    }
+    exporterMenuPdf: false
   };
 
   $scope.gridOptions.columnDefs = [{
