2b2ee8ff803c476f60de00e6cf7a7e39e5cc8d48
Dennis Ting <dennisting@gmail.com>
2016-04-09 05:13:08 -0700

    Update client to use new comparison route
    Update client to display both cfa and non cfa pie
    charts at the same time. School selector allows for
    choosing which school to show charts for or a combined
    chart.

--- client/app/main/visualization/visualization.controller.js
+++ client/app/main/visualization/visualization.controller.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var defaultChartConfig, categorySettings, dataToSeries;
+
 function VisualizationCtrl($scope, $timeout, School, Sidebar, Visualization) {
   $scope.sidebar = Sidebar;
   $scope.selected = {};
@@ -10,150 +12,112 @@ function VisualizationCtrl($scope, $timeout, School, Sidebar, Visualization) {
     $scope.schools = schools;
   });
 
-  $scope.studentType = 'nonCfa';
-  $scope.data = {
-    cfa: {
-      arca: {
-        count: 0, pct: 0
-      },
-      ca: {
-        count: 0, pct: 0
-      },
-      other: {
-        count: 0, pct: 0
-      }
-    },
-    nonCfa: {
-      arca: {
-        count: 0, pct: 0
-      },
-      ca: {
-        count: 0, pct: 0
-      },
-      other: {
-        count: 0, pct: 0
-      }
-    }
-  };
-  $scope.chartConfig = {
-    options: {
-      chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-      },
-      title: {
-        text: 'ARCA vs CA'
-      },
-      tooltip: {
-        pointFormat: '{series.name}<br><b>{point.percentage:.1f}%</b><br><em>{point.count} tot.</em>'
-      },
-      plotOptions: {
-        pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-            enabled: false,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-          },
-          showInLegend: true
-        }
-      }
-    },
+  $scope.cfaConfig = _.merge({
+    options: {title: {text: 'Enrolled in Child First Authority'}},
     func: function(chart) {
-      $scope.chartObj = chart;
+      $scope.cfaChartObj = chart;
     }
-  };
+  }, defaultChartConfig);
 
-  Visualization.arcaCa().$promise.then(function(results) {
-    function counter(rec, isCfa) {
-      var key = isCfa ? 'cfa' : 'nonCfa';
-      if(rec.arca) {
-        $scope.data[key].arca.count++;
-      } else if(rec.ca) {
-        $scope.data[key].ca.count++;
-      } else {
-        $scope.data[key].other.count++;
-      }
+
+  $scope.nonConfig = _.merge({
+    options: {title: {text: 'Not Enrolled in Child First Authority'}},
+    func: function(chart) {
+      $scope.nonChartObj = chart;
     }
-    _.forEach(results, function(record) {
-      if(record.student.cfa) {
-        counter(record, true);
-      } else {
-        counter(record, false);
-      }
-    });
+  }, defaultChartConfig);
 
-    function getTwoDecimals(n, t) {
-      return parseInt(((n / t) * 100).toFixed(2), 10);
-    }
-    _.forEach(['cfa', 'nonCfa'], function(k) {
-      var tot = $scope.data[k].arca.count + $scope.data[k].ca.count + $scope.data[k].other.count;
-      if(tot) {
-        _.forEach($scope.data[k],function(v, kB) {
-          $scope.data[k][kB].pct = getTwoDecimals($scope.data[k][kB].count, tot);
-        });
-      } else {
-        _.forEach($scope.data[k],function(v, kB) {
-          $scope.data[k][kB].pct = 0;
-        });
-      }
-    });
-
-    $scope.chartConfig.series = [{
-      name: $scope.studentType === 'cfa' ?
-        'A CFA student' : 'Not a CFA student',
-      colorByPoint: true,
-      data: [{
-          name: 'ARCA',
-          y: $scope.data[$scope.studentType].arca.pct,
-          count: $scope.data[$scope.studentType].arca.count
-        },{
-          name: 'CA',
-          y: $scope.data[$scope.studentType].ca.pct,
-          count: $scope.data[$scope.studentType].ca.count
-        }, {
-          name: 'Other',
-          y: $scope.data[$scope.studentType].other.pct,
-          count: $scope.data[$scope.studentType].other.count
-      }]
-    }];
-  });
-
-  $scope.$watch('studentType', function(n, o) {
-    if(n !== o) {
-      $scope.chartConfig.series = [{
-        name: n === 'cfa' ? 'A CFA student' : 'Not a CFA student',
-        colorByPoint: true,
-        data: [{
-            name: 'ARCA',
-            y: $scope.data[n].arca.pct,
-            count: $scope.data[n].arca.count
-          },{
-            name: 'CA',
-            y: $scope.data[n].ca.pct,
-            count: $scope.data[n].ca.count
-          }, {
-            name: 'Other',
-            y: $scope.data[n].other.pct,
-            count: $scope.data[n].other.count
-        }]
-      }];
-    }
-  });
   $scope.$watch('selected.school', function(n, o) {
     if (n !== o) {
-      Visualization.get({controller: 'cfa-comparison'}, n, function(res) {
-        console.log(res);
+      delete $scope.loaded;
+      Visualization.get({controller: 'cfa-comparison'}, n, function(data) {
+        $scope.cfaConfig.series = dataToSeries('Enrolled', data.cfa);
+        $scope.nonConfig.series = dataToSeries('Not Enrolled', data.non);
+        $scope.loaded = true;
+        $timeout(function() {
+          if ($scope.loaded) {
+            $scope.cfaChartObj.reflow();
+            $scope.nonChartObj.reflow();
+          }
+        });
+      }, function(err) {
+        console.log(err);
       });
     }
   });
   // Timeout used to redraw the chart after animation delay of resizing the
   // sidebar.
   $scope.$watch('sidebar.isCollapsed', function() {
-    $timeout(function() {$scope.chartObj.reflow();}, 510);
+    $timeout(function() {
+      if ($scope.loaded) {
+        $scope.cfaChartObj.reflow();
+        $scope.nonChartObj.reflow();
+      }
+    }, 510);
   });
 }
+
+defaultChartConfig = {
+  options: {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    tooltip: {
+      pointFormat: '{series.name}<br>' +
+                   '<b>{point.percentage:.1f}%</b><br>' +
+                   '<em>{point.y} total</em>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          color: 'black',
+          distance: -35,
+          useHTML: true,
+          formatter: function() {
+            if (this.y) {
+              return this.y + ' <i class="fa fa-child"></i><br/>' +
+                     '(' + Math.round(this.percentage * 10) / 10 + '%)';
+            }
+            return null;
+          }
+        },
+        showInLegend: true
+      },
+      series: {
+        animation: false
+      }
+    }
+  }
+};
+
+categorySettings = {
+  'Normal': {legendIndex: 0, color: '#a5c261'},
+  'ARCA': {legendIndex: 1, color: '#ffc66d'},
+  'CA': {legendIndex: 2, color: '#da4939'}
+};
+
+dataToSeries = function(name, data) {
+  var series = [{
+    name: name,
+    colorByPoint: true,
+    data: []
+  }];
+  if (data) {
+    series[0].data = _(data)
+      .toPairs()
+      .map(function(item) {
+        item.push(categorySettings[item[0]].legendIndex);
+        item.push(categorySettings[item[0]].color);
+        return _.zipObject(['name', 'y', 'legendIndex', 'color'], item);
+      })
+      .value();
+  }
+  return series;
+};
 
 angular.module('app').controller('VisualizationCtrl', VisualizationCtrl);
