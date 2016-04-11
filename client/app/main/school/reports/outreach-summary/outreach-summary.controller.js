13eead9e32de69a8f8ee10f170e47fd13e74e7c0
Dennis Ting <dennisting@gmail.com>
2016-04-11 05:09:41 -0700

    Outreach summary view now includes resolved and outstanding counts

--- client/app/main/school/reports/outreach-summary/outreach-summary.controller.js
+++ client/app/main/school/reports/outreach-summary/outreach-summary.controller.js
@@ -13,55 +13,183 @@ function OutreachSummaryCtrl($scope, $timeout, GridDefaults,
     GridDefaults.colDefs.firstName(),
     GridDefaults.colDefs.lastName(),
     {
-      name: 'totals.all',
+      name: 'Phone.count',
+      displayName: 'Phone',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Phone Calls',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
+    }, {
+      name: 'Phone.resolved',
+      displayName: 'Resolved',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Phone Resolved',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'Phone.outstanding',
+      displayName: 'Outstanding',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Phone Outstanding',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'Letter.count',
+      displayName: 'Letter',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Letters Sent',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
+    }, {
+      name: 'Letter.resolved',
+      displayName: 'Resolved',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Letter Resolved',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'Letter.outstanding',
+      displayName: 'Outstanding',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Letter Outstanding',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'Home.count',
+      displayName: 'Home',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Home Visits',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
+    }, {
+      name: 'Home.resolved',
+      displayName: 'Resolved',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Home Resolved',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'Home.outstanding',
+      displayName: 'Outstanding',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Home Outstanding',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'SST.count',
+      displayName: 'SST',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'SST Referrals',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
+    }, {
+      name: 'SST.resolved',
+      displayName: 'Resolved',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'SST Resolved',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'SST.outstanding',
+      displayName: 'Outstanding',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'SST Outstanding',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'Court.count',
+      displayName: 'Court',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Court Referrals',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
+    }, {
+      name: 'Court.resolved',
+      displayName: 'Resolved',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Court Resolved',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'Court.outstanding',
+      displayName: 'Outstanding',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Court Outstanding',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
+    }, {
+      name: 'count',
       displayName: 'Total',
       minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Total Outreaches',
       treeAggregationType: uiGridGroupingConstants.aggregation.SUM
     }, {
-      name: 'totals["Phone Call"] || 0',
-      displayName: 'Calls',
-      minWidth: 100,
-      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
+      name: 'resolved',
+      displayName: 'Resolved',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Resolved Outreaches',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
     }, {
-      name: 'totals["Letter Sent"] || 0',
-      displayName: 'Letters',
-      minWidth: 100,
-      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
-    }, {
-      name: 'totals["Home Visit"] || 0',
-      displayName: 'Visits',
-      minWidth: 100,
-      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
-    }, {
-      name: 'totals["SST Referral"] || 0',
-      displayName: 'SST',
-      minWidth: 100,
-      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
-    }, {
-      name: 'totals["Court Referral"] || 0',
-      displayName: 'Court',
-      minWidth: 100,
-      treeAggregationType: uiGridGroupingConstants.aggregation.SUM
+      name: 'outstanding',
+      displayName: 'Outstanding',
+      minWidth: 80,
+      type: 'number',
+      headerTooltip: 'Outstanding Outreaches',
+      treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+      visible: false
     },
+    GridDefaults.colDefs.cfa(),
     GridDefaults.colDefs.withdrawn($scope)
   ];
   $scope.gridOptions.onRegisterApi = function(gridApi) {
     $scope.gridApi = gridApi;
-    gridApi.edit.on.afterCellEdit($scope, function(rowEntity, colDef, n, o) {
+    gridApi.edit.on.afterCellEdit($scope, function(row, colDef, n, o) {
       if (n !== o) {
         switch (colDef.name) {
+          case 'student.cfa':
+            Student.updateCFA(row.student);
+            break;
           case 'student.withdrawn':
-            Student.updateWithdrawn(rowEntity.student);
+            Student.updateWithdrawn(row.student);
             break;
         }
       }
     });
+    var totalsDefault = {
+      Phone: {count: 0, resolved: 0, outstanding: 0},
+      Letter: {count: 0, resolved: 0, outstanding: 0},
+      Home: {count: 0, resolved: 0, outstanding: 0},
+      SST: {count: 0, resolved: 0, outstanding: 0},
+      Court: {count: 0, resolved: 0, outstanding: 0}
+    };
     $scope.gridOptions.data = Student.outreachSummary();
     $scope.gridOptions.data.$promise.then(function(data) {
-      // Convert counts array to object, generate total intervention property
+      // Convert counts array to object, generate total intervention property.
       _.forEach(data, function(row) {
-        row.totals = _(row.counts).keyBy('type').mapValues('count').value();
-        row.totals.all = _.sumBy(row.counts, 'count');
+        var totals = _(row.counts)
+          .keyBy('type')
+          .mapKeys(function(value, key) {
+            return key.split(' ')[0];
+          })
+          .defaults(totalsDefault)
+          .value();
+        totals.count = _.sumBy(row.counts, 'count');
+        totals.resolved = _.sumBy(row.counts, 'resolved');
+        totals.outstanding = _.sumBy(row.counts, 'outstanding');
+        _.assign(row, totals);
       });
       // NOTE: Hack to default to expanded rows on initial load.
       // https://github.com/angular-ui/ui-grid/issues/3841
--- server/api/student/student.controller.js
+++ server/api/student/student.controller.js
@@ -160,23 +160,49 @@ exports.outreachCounts = function(req, res) {
  * - manager+ will get outreach summary for all schools
  */
 exports.outreachSummary = function(req, res) {
+  var match = {};
+  if (req.user.role === 'teacher') {
+    match.school = req.user.assignment;
+  }
   var pipeline = [{
+    $match: match
+  }, {
+    $group: {
+      _id: {school: '$school', schoolYear: '$schoolYear'},
+      outreaches: {$push: '$$ROOT'}
+    }
+  }, {
+    $sort: {'_id.schoolYear': -1}
+  }, {
+    $unwind: '$outreaches'
+  }, {
+    $project: {
+      _id: false,
+      student: '$outreaches.student',
+      type: '$outreaches.type',
+      school: '$outreaches.school',
+      resolved: {$cond: [{$gt: ['$outreaches.actionDate', 0]}, 1, 0]}
+    }
+  }, {
     $group: {
       _id: {student: '$student', type: '$type', school: '$school'},
-      count: {$sum: 1}
+      count: {$sum: 1},
+      resolved: {$sum: '$resolved'}
     }
   }, {
     $group: {
       _id: '$_id.student',
       school: {$first: '$_id.school'},
-      counts: {$push: {type: '$_id.type', count: '$count'}}
+      counts: {$push: {
+        type: '$_id.type',
+        count: '$count',
+        resolved: '$resolved',
+        outstanding: {$subtract: ['$count', '$resolved']}
+      }}
     }
   }, {
-    $project: {_id: 0, student: '$_id', counts: 1, school: 1}
+    $project: {_id: false, student: '$_id', counts: 1, school: 1}
   }];
-  if (req.user.role === 'teacher') {
-    pipeline.unshift({$match: {school: req.user.assignment}});
-  }
   Outreach.aggregate(pipeline, function(err, results) {
     if (err) handleError(res, err);
     Outreach.populate(results, [{
