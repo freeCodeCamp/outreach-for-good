e15a39118dee513e62ef4bee708c0989d45e7392
Chad Sheets <cjsheets@gmail.com>
2017-02-28 22:36:52 -0800

    Admin page (#17)
    * reafactoring prior to starting work
    
    * fixed history fallback
    
    * split into pure functional components

--- client/components/admin/AdminPage.js
+++ client/components/admin/AdminPage.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import * as userActions from '../../actions/userActions';
+
+import Dimensions from 'react-dimensions';
+import AdminTable from './AdminTable';
 
 class AdminPage extends React.Component {
+
   render() {
     return (
-            <div>
-                <h1>Dashboard</h1>
-                <p>This is a sample about page s</p>
-
-            </div>
-      );
+        <AdminTable
+          data={this.props.users}
+          width={this.props.containerWidth}
+          height={this.props.containerHeight - 55}
+        />
+    );
   }
 }
 
-export default AdminPage;
+AdminPage.propTypes = {
+  actions         : PropTypes.object.isRequired,
+  users           : PropTypes.array.isRequired,
+  containerWidth  : PropTypes.number.isRequired,
+  containerHeight : PropTypes.number.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    users : state.users
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions : bindActionCreators(userActions, dispatch)
+  };
+}
+
+//https://github.com/digidem/react-dimensions/issues/44
+export default connect(mapStateToProps, mapDispatchToProps)(
+  Dimensions({elementResize: true})(AdminPage));
+
