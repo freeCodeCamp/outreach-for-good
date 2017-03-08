230599567dd5fbc548dd6f2d834742d7bf34f565
Chad Sheets <cjsheets@gmail.com>
2017-03-08 09:20:02 -0800

    add me to session

--- client/actions/sessionActions.js
+++ client/actions/sessionActions.js
@@ -1,12 +1,28 @@
 import * as types from './actionTypes';
+import userAPI from '../api/mockUsersApi';
 import cookies from 'browser-cookies';
 
+export function loadMeSuccess(token, me) {
+  return {type: types.SESSION_VALID, token, me};
+}
+
 export function validate() {
-  return {type: types.SESSION_VALID, token: cookies.get('token')};
+  let token = cookies.get('token');
+  if(token) {
+    // Thunk returns a function that accepts a dispatch
+    return function(dispatch) {
+      return userAPI.getMe().then(me => {
+        dispatch(loadMeSuccess(token, me));
+      })
+      .catch(err => {
+        throw err;
+      });
+    };
+  }
+  return {type: types.SESSION_CLEAR};
 }
 
 export function logout() {
   cookies.erase('token');
   return {type: types.SESSION_CLEAR};
 }
-
