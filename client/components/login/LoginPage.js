f277e73273f908a4b7fb77d7947f2d18fbd8da35
Jordan Rhea <rheajt@gmail.com>
2017-03-10 21:31:35 +0300

    fixed merges

--- client/components/login/LoginPage.js
+++ client/components/login/LoginPage.js
@@ -56,9 +56,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-<<<<<<< HEAD
 export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
-
-=======
-export default connect(null, mapDispatchToProps)(LoginPage);
->>>>>>> 9bedce5c37164630dd0318d3cdec4f043a2dd666
