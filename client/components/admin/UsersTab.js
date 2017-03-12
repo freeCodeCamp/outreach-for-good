8353a0e5393f0222cb558b42bcb038daea53657a
Chad Sheets <cjsheets@gmail.com>
2017-03-11 16:27:33 -0800

    added admin page tabs

---
+++ client/components/admin/UsersTab.js
@@ -0,0 +1,51 @@
+import React, {PropTypes} from 'react';
+import DataTable from './DataTable';
+
+const UsersTab = ({view, users}) => {
+  const columnDefs = [{
+    title : 'Name',
+    id    : 'name',
+    fixed : true
+  }, {
+    title    : 'Email Address',
+    id       : 'email',
+    flexGrow : 1
+  }, {
+    title    : 'Assigned School',
+    id       : 'school',
+    flexGrow : 1
+  }, {
+    title    : 'Role',
+    id       : 'role',
+    flexGrow : 1
+  }, {
+    title    : 'Actions',
+    id       : 'action',
+    width : 100
+  }];
+
+  const tableProps = {
+    table : {
+      width  : view.width,
+      height : view.height,
+    },
+    rowHeight    : 50,
+    headerHeight : 50
+  };
+
+  return (
+    <DataTable
+      table={tableProps}
+      column={columnDefs}
+      data={users}
+    />
+  );
+};
+
+UsersTab.propTypes = {
+  view  : PropTypes.object.isRequired,
+  users : PropTypes.array.isRequired,
+};
+
+export default UsersTab;
+
