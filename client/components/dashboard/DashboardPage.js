b68cce96be8726a8430bae9d78ff511a3ce10c97
Chad Sheets <cjsheets@gmail.com>
2017-03-12 10:33:26 -0700

    added tabs

--- client/components/dashboard/DashboardPage.js
+++ client/components/dashboard/DashboardPage.js
@@ -1,14 +1,103 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import * as userActions from '../../actions/userActions';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+import Dimensions from 'react-dimensions';
+
+import CourtTab from './CourtTab';
+import HomeTab from './HomeTab';
+import LetterTab from './LetterTab';
+import PhoneTab from './PhoneTab';
+import SstTab from './SstTab';
+import StudentTab from './StudentTab';
 
 class DashboardPage extends React.Component {
+
   render() {
     return (
-      <div>
-        <h1>Dashboard</h1>
-        <p>This is a sample about page!</p>
-      </div>
+      <Tabs
+        style={{width: this.props.containerWidth}}
+      >
+        <Tab label="SST">
+          <StudentTab
+            view = {{
+              width  : this.props.containerWidth,
+              height : this.props.containerHeight - 48
+            }}
+            schools = {this.props.users}
+          />
+        </Tab>
+        <Tab label="SST">
+          <PhoneTab
+            view = {{
+              width  : this.props.containerWidth,
+              height : this.props.containerHeight - 48
+            }}
+            schools = {this.props.users}
+          />
+        </Tab>
+        <Tab label="SST">
+          <LetterTab
+            view = {{
+              width  : this.props.containerWidth,
+              height : this.props.containerHeight - 48
+            }}
+            schools = {this.props.users}
+          />
+        </Tab>
+        <Tab label="SST">
+          <HomeTab
+            view = {{
+              width  : this.props.containerWidth,
+              height : this.props.containerHeight - 48
+            }}
+            schools = {this.props.users}
+          />
+        </Tab>
+        <Tab label="SST">
+          <SstTab
+            view = {{
+              width  : this.props.containerWidth,
+              height : this.props.containerHeight - 48
+            }}
+            schools = {this.props.users}
+          />
+        </Tab>
+        <Tab label="Court">
+          <CourtTab
+            view = {{
+              width  : this.props.containerWidth,
+              height : this.props.containerHeight - 48
+            }}
+            schools = {this.props.users}
+          />
+        </Tab>
+      </Tabs>
     );
   }
 }
 
-export default DashboardPage;
+DashboardPage.propTypes = {
+  actions         : PropTypes.object.isRequired,
+  users           : PropTypes.array.isRequired,
+  containerWidth  : PropTypes.number.isRequired,
+  containerHeight : PropTypes.number.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    users : state.users
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions : bindActionCreators(userActions, dispatch)
+  };
+}
+
+//https://github.com/digidem/react-dimensions/issues/44
+export default connect(mapStateToProps, mapDispatchToProps)(
+  Dimensions({elementResize: true})(DashboardPage));
