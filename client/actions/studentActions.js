505b7c9cc032f5a5e3cd46c8afa74caf077e719d
Jordan Rhea <rheajt@gmail.com>
2017-03-19 19:41:14 +0300

    records page needs to handle various roles of the users and deal with errors such as uploading a record twice in a day

--- client/actions/studentActions.js
+++ client/actions/studentActions.js
@@ -1,8 +1,16 @@
 import * as types from './actionTypes';
+import StudentApi from '../api/StudentApi';
 
 export function fetchStudent(studentId) {
+  return dispatch => StudentApi.fetchStudent(studentId)
+    .then(student => dispatch({
+      type : types.FETCH_STUDENT_SUCCESS,
+      student
+    }));
+}
+
+export function unmountStudent() {
   return {
-    type    : types.FETCH_STUDENT,
-    payload : studentId
+    type : types.UNMOUNT_STUDENT
   };
 }
---
+++ client/api/Api.js
@@ -0,0 +1,38 @@
+class Api {
+  /**
+   * Retrieves JWT token for authorization
+   */
+  static requestHeaders() {
+    //console.log('Setup Header', sessionStorage.token);
+    return {Authorization: `Bearer ${sessionStorage.token}`};
+  }
+
+  /**
+   * Process API response codes
+   */
+  static parseResponse(res) {
+    let status = res.status;
+    if(status >= 400 && status < 500) {
+      throw res;
+    } else if(status >= 300 && status < 400) {
+      throw res;
+    } else {
+      return res.json();
+    }
+  }
+
+  /**
+   * Handle errors from api calls
+   */
+  static handleError(err) {
+    console.log(err);
+  }
+
+  /**
+   * Fetch requests
+   */
+  static fetchRequest(method, url, body) {
+    console.log(method, url, body);
+  }
+}
+export default Api;
