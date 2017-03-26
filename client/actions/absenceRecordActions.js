1b56aa8d6cef2749f5c1d705efa8a61e62b0979c
Chad Sheets <cjsheets@gmail.com>
2017-03-25 22:16:35 -0700

    added framework for absence record actions

---
+++ client/actions/absenceRecordActions.js
@@ -0,0 +1,154 @@
+import * as types from './actionTypes';
+import { validate } from './sessionActions';
+import AbsenceRecordsApi from '../api/AbsenceRecordsAltApi';
+
+export function loadRecordsSuccess(users) {
+  return {type: types.LOAD_ABSENCE_RECORD_SUCCESS, users};
+}
+
+// Functions below handle asynchronous calls.
+// Each returns a function that accepts a dispatch.
+// These are used by redux-thunk to support asynchronous interactions.
+
+/**
+ * Get current absence records.
+ *   - untested
+ */
+export function fetchRecords() {
+  return function(dispatch) {
+    return AbsenceRecordsApi.fetchRecords().then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Get the absence record of a particular student
+ *   - untested
+ */
+export function fetchStudentRecord(studentId) {
+  return function(dispatch) {
+    return AbsenceRecordsApi.fetchStudentRecord(studentId).then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Get entries from current absence records.
+ *   - untested
+ */
+export function fetchRecordsList() {
+  return function(dispatch) {
+    return AbsenceRecordsApi.fetchRecordsList().then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Get list of absence records for the most recent
+ *   - untested
+ */
+export function fetchSchoolRecordList(schoolId) {
+  return function(dispatch) {
+    return AbsenceRecordsApi.fetchSchoolRecordList(schoolId).then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Get entries for students with outreaches specified by filter.
+ *   - untested
+ */
+export function fetchRecordsListQuery() {
+  return function(dispatch) {
+    return AbsenceRecordsApi.fetchRecordsListQuery().then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Get entries for students at risk of becoming chronically absent.
+ *   - untested
+ */
+export function fetchRecordsListAtRisk() {
+  return function(dispatch) {
+    return AbsenceRecordsApi.fetchRecordsListAtRisk().then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Get entries of chronically absent students.
+ *   - untested
+ */
+export function fetchRecordsListChronic() {
+  return function(dispatch) {
+    return AbsenceRecordsApi.fetchRecordsListChronic().then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Get entries from specified absence record year.
+ *   - untested
+ */
+export function fetchRecordsListYear(year) {
+  return function(dispatch) {
+    return AbsenceRecordsApi.fetchRecordsListYear(year).then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Creates a new absence record in the DB.
+ *   - untested
+ */
+export function addRecord(schoolId, record) {
+  return function(dispatch) {
+    return AbsenceRecordsApi.addRecord(schoolId, record).then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Delete an absence record
+ *   - untested
+ */
+export function removeRecord(recordId) {
+  return function(dispatch) {
+    return AbsenceRecordsApi.removeRecord(recordId).then(res =>
+      dispatch(loadRecordsSuccess(res))
+    )
+    .catch(err => handleError(err, dispatch));
+  };
+}
+
+/**
+ * Handle expected return codes
+ */
+export function handleError(err, dispatch) {
+  let status = err.status;
+  console.log('In userActions.js, handleError()', status, err);
+  if(status == 401) {
+    return dispatch(validate());
+  } else {
+    throw err;
+  }
+}
