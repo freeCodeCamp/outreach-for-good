b4cd86dcbe0b28d1f78df1107e48b39193ae5f70
jordan rhea <rheajt@gmail.com>
2017-03-29 23:23:08 +0300

    starting to get a sense of how to work immutable objects into the manage tab of the records page but i am throwing new errors when routing to the student page

--- client/components/records/RecordsPage.js
+++ client/components/records/RecordsPage.js
@@ -6,22 +6,61 @@ import * as schoolActions from '../../actions/schoolActions';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import UploadTab from './partials/UploadTab';
 import ManageTab from './partials/ManageTab';
+import Dimensions from 'react-dimensions';
+import TableModel from '../../models/TableModel';
+import * as absAct from '../../actions/absenceRecordActions';
 
+const table = new TableModel();
 
 class RecordsPage extends Component {
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
 
+    let nextTable = this.initializeTable();
     this.state = {
-      currentTab : 'upload'
+      table         : nextTable,
+      currentTab    : 'upload',
+      manageRecords : props.absenceRecords
     };
 
+    this.initializeTable = this.initializeTable.bind(this);
+    this.manageTabClick = this.manageTabClick.bind(this);
     this.changeTab = this.changeTab.bind(this);
     this.confirm = this.confirm.bind(this);
     this.manageRecord = this.manageRecord.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    let nextTable = this.state.table;
+    nextTable = table.updateSortIndex(nextTable, '');
+    nextTable = table.buildIndexMap(nextTable, nextProps.absenceRecords);
+
+    this.setState({
+      table : nextTable
+    });
+  }
+
+  /**
+   * Initialize Data Table
+   *   - Retrieve and configure data for table
+   *   - Set default state for 'action' variables
+   */
+  initializeTable() {
+    let nextTable;
+
+    //this fetches the school records list for the manage tab
+    //currently hardcoded in
+    //need to work out the loading order to resolve without errors
+    let schoolId = '58dbbc071f99bcb70a9d0958';
+    this.props.absActions.fetchSchoolRecordList(schoolId);
+
+    nextTable = table.setSelectedTab(table, 'manage');
+    return nextTable;
+  }
+
   componentWillMount() {
+    //TODO relook at these
+    //which of these is unneeded
     this.props.schoolActions.getAllSchools();
     this.props.recordsActions.getCurrentRecord();
   }
@@ -40,9 +79,15 @@ class RecordsPage extends Component {
     this.props.recordsActions.getSchoolRecordList(schoolId);
   }
 
+  manageTabClick(action, data, event) {
+    let manageRecords = this.props.absenceRecords.get(data).get('entries');
+    this.setState({ manageRecords });
+  }
+
   render() {
     return (
       <Tabs
+        style={{width: this.props.containerWidth}}
         value={this.state.currentTab}
         onChange={this.changeTab}
         >
@@ -59,7 +104,15 @@ class RecordsPage extends Component {
           label="Manage"
           value="manage">
           <ManageTab
+            view={{
+              width  : this.props.containerWidth - 20,
+              height : this.props.containerHeight - 48 - 80
+            }}
+            records={this.props.absenceRecords}
+            table={this.state.table}
             schools={this.props.schools}
+            manageRecords={this.state.manageRecords}
+            clickHandler={this.manageTabClick}
           />
         </Tab>
       </Tabs>
@@ -75,17 +128,19 @@ RecordsPage.propTypes = {
 
 function mapStateToProps(state) {
   return {
-    session : state.session,
-    records : state.records,
-    schools : state.schools
+    absenceRecords : state.absenceRecords,
+    session        : state.session,
+    records        : state.records,
+    schools        : state.schools
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
+    absActions     : bindActionCreators(absAct, dispatch),
     recordsActions : bindActionCreators(recordsActions, dispatch),
     schoolActions  : bindActionCreators(schoolActions, dispatch)
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(RecordsPage);
+export default connect(mapStateToProps, mapDispatchToProps)(Dimensions({elementResize: true})(RecordsPage));
--- client/reducers/absenceRecordReducer.js
+++ client/reducers/absenceRecordReducer.js
@@ -2,6 +2,7 @@ import * as types from '../actions/actionTypes';
 
 import { List, fromJS } from 'immutable';
 import AbsenceRecord from '../models/AbsenceRecordModel';
+import AbsenceRecordListModel from '../models/AbsenceRecordListModel';
 
 const initialState = new List();
 
@@ -11,7 +12,9 @@ export default (state = initialState, action) => {
   case types.LOAD_ABSENCE_RECORD_SUCCESS:
     return fromJS(action.absenceRecords)
       .map(record => new AbsenceRecord(record));
-
+  case types.LOAD_SCHOOL_RECORD_LIST_SUCCESS:
+    return fromJS(action.recordList)
+      .map(recordList => new AbsenceRecordListModel(recordList));
   default:
     return state;
   }
