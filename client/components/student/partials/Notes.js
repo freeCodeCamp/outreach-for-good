e8d82409098bd39e543ee57e4de3945af6e5148b
jordan rhea <rheajt@gmail.com>
2017-04-11 22:38:04 +0300

    merged in the changes that I made to the student page

--- client/components/student/partials/Notes.js
+++ client/components/student/partials/Notes.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Chip from 'material-ui/Chip';
+import Avatar from 'material-ui/Avatar';
 
-const Notes = ({ notes }) =>
+const styles = {
+  chip : {
+    margin : 4,
+  },
+  wrapper : {
+    display  : 'flex',
+    flexWrap : 'wrap',
+  },
+};
+
+const Notes = ({ notes, postNote, editNote }) =>
   <div>
-    <TextField />
+    <form onSubmit={postNote}>
+      <TextField
+        id="post-note-field"
+        hintText="Type your note here"/>
+      <RaisedButton
+        label="Submit"
+        type="submit"
+        primary
+      />
+    </form>
     {notes
-      && <ul>
-        {notes.map(note => console.log(note))}
-      </ul>
-    }
+      && notes.map((note, i) =>
+      <div key={i} style={styles.wrapper}>
+        <Chip
+          onTouchTap={editNote}
+          style={styles.chip}
+        >
+          <Avatar src={note.user.google.image.url} />
+          {note.user.google.displayName}: {note.note}
+        </Chip>
+      </div>
+    )}
   </div>
 ;
 export default Notes;
