7f7b883938d824227e13860baecf3e5325d1cdf5
jordan rhea <rheajt@gmail.com>
2017-04-20 20:44:54 +0300

    started working on the school settings page

---
+++ client/components/common/SchoolSelect.js
@@ -0,0 +1,27 @@
+import React, {PropTypes} from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+const SchoolSelect = ({ schools, value, changeSchool }) =>
+  <SelectField
+    floatingLabelText="Select a school..."
+    value={value}
+    onChange={changeSchool}
+    fullWidth
+    >
+    {schools.map((school, i) =>
+      <MenuItem
+        key={i}
+        value={school}
+        primaryText={school.name} />
+      )}
+  </SelectField>
+;
+
+SchoolSelect.propTypes = {
+  schools        : PropTypes.object,
+  selectedSchool : PropTypes.object,
+  changeSchool   : PropTypes.func
+};
+
+export default SchoolSelect;
