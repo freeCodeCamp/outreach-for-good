0f0c6c7918869e53e6cd278c08c4baff8dbbf71e
jordan rhea <rheajt@gmail.com>
2017-05-31 20:40:19 +0300

    continued to update the outreach cards to more closely match the original

--- client/components/student/partials/Outreaches.js
+++ client/components/student/partials/Outreaches.js
@@ -1,61 +1,45 @@
 import React, {PropTypes} from 'react';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
-import Chip from 'material-ui/Chip';
 import FlatButton from 'material-ui/FlatButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import TextField from 'material-ui/TextField';
 import DatePicker from 'material-ui/DatePicker';
 
-// import StudentCard from './StudentCard';
-
-const cardStyle = {
-  width         : '50%',
-  display       : 'inline-block',
-  height        : '400px',
-  verticalAlign : 'top'
-};
-
-const wrapper = {
-  display  : 'flex',
-  flexWrap : 'wrap'
-};
-
-const Outreaches = ({ outreaches, outreachNote, outreachAction }) =>
-<div className="outreach-cards">
-    {outreaches.map((outreach, i) =>
-    <Card
-      key={i}
-      style={cardStyle}>
-      <CardHeader
-        title={`${outreach.type} (${outreach.absences} absences)`}
-        subtitle={`Triggered: ${new Date(outreach.triggerDate).toDateString()}`} />
-      <CardText>
-        <DatePicker
-          onChange={outreachAction}
+const Outreaches = ({ outreach, outreachId, outreachNote, outreachAction }) =>
+<Card className="outreach-card">
+  <CardHeader
+    title={`${outreach.type} (${outreach.absences} absences)`}
+    subtitle={`Triggered: ${new Date(outreach.triggerDate).toDateString()}`} />
+  <CardText>
+    <div className="outreach-card-text-container">
+      <div className="left-column">
+        <DatePicker id={outreachId}
+          onTouchTap={outreachAction}
           hintText="Date Action Taken"/>
-        <form id={outreach._id}
-          onSubmit={outreachNote}>
+      </div>
+      <div className="right-column">
+        <form
+          onSubmit={outreachNote}
+          id={outreachId}>
           <TextField
             hintText="Add a note to this outreach"
             name="outreachNote" />
-          <FlatButton
-            icon={<ContentAdd />}
-            type="submit" />
+            <FlatButton
+              icon={<ContentAdd />}
+              type="submit" />
         </form>
-        <div >
-          {outreach.notes.map((note, j) =>
-            <span style={wrapper} key={j}>
-              <Chip>
-                {new Date(note.date).toDateString()}
-              </Chip>
-              {note.note}
-            </span>
+        <div className="outreach-notes">
+          {outreach.notes.map((note, i) =>
+          <p key={i}>
+            <b>({new Date(note.date).toDateString()})</b>
+            &nbsp; {note.note}
+          </p>
           )}
         </div>
-      </CardText>
-    </Card>
-    )}
-  </div>;
+      </div>
+    </div>
+  </CardText>
+</Card>;
 
 Outreaches.propTypes = {
   outreaches : PropTypes.array
