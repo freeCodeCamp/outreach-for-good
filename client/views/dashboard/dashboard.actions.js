365aeef8739a77e965c95910c1f8422fa2bf1976
Chad Sheets <cjsheets@gmail.com>
2017-07-30 20:44:14 -0700

    Dashboard - cloned basic functionality (#136)
    * update eslint settings
    
    * update function style
    
    * enable filter button
    
    * improve render control for datatable
    
    * fix row selection
    
    * fix wrong this reference
    
    * added prop-types package
    
    * corrected load order
    
    * feedback when no results available
    
    * add api calls to dashboard
    
    * move tabs into component
    
    * convert tabs to PFC
    
    * refactor buttons
    
    * fixed width-height
    
    * major refactor
    
    * refactor admin page
    
    * corrected formatting
    
    * fixed typos
    
    * comment out link
    
    * fixed filter, select, collapse
    
    * sorting fix
    
    * renamed datatable, fixed null references
    
    * add minus when collapsed
    
    * add grade id
    
    * start collapsed
    
    * add reducer for withdrawn students
    
    * add checkbox to dashboard
    
    * basic Edit API calls
    
    * show withdrawn students
    
    * automatic reloading after request
    
    * remove console.log
    
    * checks and dates for data table

--- client/views/dashboard/dashboard.actions.js
+++ client/views/dashboard/dashboard.actions.js
@@ -2,24 +2,16 @@
 export const FILTER = 'FILTER';
 export const EDIT = 'EDIT';
 
-// Open <Dialog>s
-export const WITHDRAW_STUDENT = 'WITHDRAW_STUDENT';
-export const ENROLL_STUDENT = 'ENROLL_STUDENT';
-
 // General Actions
-export const IEP_ADD = 'WITHDRAW_STUDENT';
-export const IEP_REMOVE = 'ENROLL_STUDENT';
-export const CFA_ADD = 'WITHDRAW_STUDENT';
-export const CFA_REMOVE = 'ENROLL_STUDENT';
+export const IEP_ADD = 'IEP_ADD';
+export const IEP_REMOVE = 'IEP_REMOVE';
+export const CFA_ADD = 'CFA_ADD';
+export const CFA_REMOVE = 'CFA_REMOVE';
+export const WITHDRAW_ADD = 'WITHDRAW_ADD';
+export const WITHDRAW_REMOVE = 'WITHDRAW_REMOVE';
 
 // API Actions
 export const TOGGLE_WITHDRAWN_STUDENTS = 'TOGGLE_WITHDRAWN_STUDENTS';
 export const ALL_YEARS = 'ALL_YEARS';
 export const Y2016_Y2017 = 'Y2016_Y2017';
 export const Y2015_Y2016 = 'Y2015_Y2016';
-
-// This list helps keep if-statements shorter
-export const DIALOG_LIST = [
-  'WITHDRAW_STUDENT',
-  'ENROLL_STUDENT'
-];
