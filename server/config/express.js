222c9aa17503af8563e5782d6cb21aeb674ce070
Chad Sheets <cjsheets@gmail.com>
2017-09-19 07:46:58 -0800

    fix favicon crash (#178)

--- server/config/express.js
+++ server/config/express.js
@@ -30,7 +30,7 @@ module.exports = function(app) {
   app.use(cookieParser());
   app.use(passport.initialize());
   if(env === 'production') {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    //app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', path.join(config.root, 'public'));
     app.use(morgan('dev'));
