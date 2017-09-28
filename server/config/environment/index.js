2095d135e7619326a2e0c4212b3c8266f743b4fc
Chad Sheets <cjsheets@gmail.com>
2017-09-27 18:52:58 -0800

    consistant port 8000 (#191)

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -22,7 +22,7 @@ var all = {
   root: process.env.APP_ROOT || path.normalize(__dirname + '/../../..'),
 
   // Server port
-  port: process.env.PORT || 9000,
+  port: process.env.PORT || 8000,
 
   // Server IP
   ip: process.env.IP || '0.0.0.0',
